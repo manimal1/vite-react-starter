36815adc046171adca36de669f587ec8e89d43ba
Jeremiah McCurdy <jeremiah.mccurdy@gmail.com>
2022-04-13 16:20:55 +0200

    set up testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+
+interface Config extends UserConfig {
+  test: {
+    globals: boolean;
+    environment: string;
+    setupFiles: string;
+  };
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/vite-react-starter/',
   plugins: [react(), tsconfigPaths()],
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTests.js',
+  },
+} as Config);
